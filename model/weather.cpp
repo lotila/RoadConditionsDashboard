1a68af13ba4438ea81e6f5e5ada589b1d4f2ae37
Ile <ilmari.marttila@gmail.com>
2022-09-29 23:11:49 +0300

    Request wind speed from Nekala and show it

--- model/weather.cpp
+++ model/weather.cpp
@@ -1,6 +1,14 @@
 #include "weather.h"
 
 #include "util.h"
+#include "APIC/curlutil.h"
+
+#include <curl/curl.h>
+#include "tinyxml/tinyxml2.h"
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
 
 namespace models
 {
@@ -8,24 +16,52 @@ namespace models
 Weather::Weather()
 {
     // TODO
+    this->updateWeather();
 }
 
-float Weather::avgWind(const util::Coord& coord, const util::TimeSlot& time)
+const std::string &Weather::avgWind(const util::Coord& coord, const util::TimeSlot& time)
 {
-    // TODO
     return this->wind;
 }
 
 void Weather::updateWeather()
 {
-    if (this->wind < 5)
-    {
-        this->wind = 42;
+    CURL *curl_handle;
+    CURLcode res;
+
+    struct MemoryStruct chunk;
+    chunk.memory = (char*)malloc(1);
+    chunk.size = 0;
+
+    curl_handle = curl_easy_init();
+    if(curl_handle) {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=livi::observations::road::default::timevaluepair&place=hervanta&parameters=windspeedms&");
+        curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "salamyhkainen_agentti");
+
+        res = curl_easy_perform(curl_handle);
+
+        if(res != CURLE_OK) {
+            std::cerr << "fuck: " << curl_easy_strerror(res) << std::endl;
+            this->wind = "virhes";
+        } else {
+            tinyxml2::XMLDocument document(false);
+            document.Parse(chunk.memory, chunk.size);
+            std::string dataAsText = document.RootElement()
+                                        ->FirstChildElement("wfs:member")
+                                        ->FirstChildElement("omso:PointTimeSeriesObservation")
+                                        ->FirstChildElement("om:result")
+                                        ->FirstChildElement("wml2:MeasurementTimeseries")
+                                        ->LastChildElement("wml2:point")
+                                        ->FirstChildElement("wml2:MeasurementTVP")
+                                        ->FirstChildElement("wml2:value")->GetText();
+            this->wind = "nyt tuuloo " + dataAsText + " m/s";
+        }
+        curl_easy_cleanup(curl_handle);
     }
-    else
-    {
-        this->wind = 4.7;
-    }
+    free(chunk.memory);
 }
 
 }
